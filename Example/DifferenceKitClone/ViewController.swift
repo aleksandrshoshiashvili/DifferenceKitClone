3921504b167dabccd9bb41f193cdaebd31a761ca
Alexander Shoshiashvili <aleksandr.shoshiashvili@gmail.com>
2020-05-08 17:53:18 +0200

    Initial commit

---
+++ Example/DifferenceKitClone/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  DifferenceKitClone
+//
+//  Created by aleksandrshoshiashvili on 05/08/2020.
+//  Copyright (c) 2020 aleksandrshoshiashvili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
